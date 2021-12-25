f875ebb9be72c29aa4840e03fdf1ef9e07f0305c
Abass14 <abasstoy14@gmail.com>
2021-12-26 00:02:25 +0100

    row and column

--- app/src/main/java/com/example/composecourse/MainActivity.kt
+++ app/src/main/java/com/example/composecourse/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.composecourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composecourse.ui.theme.ComposeCourseTheme
 
@@ -14,7 +19,41 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "Abass")
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(0.5f)
+                        .background(Color.Green),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = "Hello")
+                    Text(text = "Hi")
+                    Text(text = "Hello")
+                    Text(text = "Hi")
+                    Text(text = "Hello")
+                    Text(text = "Hi")
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                        .background(Color.Blue),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = "Hello")
+                    Text(text = "Hi")
+                    Text(text = "Hello")
+                    Text(text = "Hi")
+                    Text(text = "Hello")
+                    Text(text = "Hi")
+                }
+            }
+
         }
     }
 }
@@ -24,8 +63,8 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Greeting("Android")
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    Greeting("Android")
+//}
